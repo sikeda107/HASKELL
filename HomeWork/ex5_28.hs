96ac179b110895bb34b7317c96c7dadd56ead9bd
Sho Ikeda <wakame1121@gmail.com>
2018-01-11 14:36:06 +0900

    modify 5_28

--- HomeWork/ex5_28.hs
+++ HomeWork/ex5_28.hs
@@ -19,18 +19,6 @@ borrowed db bk = or [j == bk | j <- bkdb]
 numBorrowed :: Database -> Person -> Int
 numBorrowed db pn = length [j | j <- db , fst j == pn ]
 
-  -- borrowed 以外はオーケーです。borrowed の戻り値は Bool です（教科書を
-  -- 参照のこと）。引数に指定された本が貸出中ならTrue、そうでなければ
-  -- Falseを返します。そのようにするには、池田さんが書いた borrowed の
-  -- 定義をほんのすこしだけ変更するだけでオーケーです。正しい borrowed を
-  -- 書いて再提出してください。
-  --
-  -- borrowers は、fst や snd を使う代わりにパターンマッチを使えば、
-  -- より簡潔に書けます。また、numBorrwed は where を使って前処理を
-  -- しなくても、データベースから取り出したペアの第一要素と pn を
-  -- 比べれば簡単に書けます。
-
-
 main :: IO ()
 main = do
  print $ borrowers exampleBase "Tintin"
--- README.md
+++ README.md
@@ -11,9 +11,10 @@ Prelude> :load Hello
 ```
 ### スクリプト
 `user$ runghc Hello.hs hoge huga`
+### QuickCheckパッケージ確認
+`user$ ghc-pkg list QuickCheck`
 ## Practice
 Hello.hs - [参考URL](http://yamaimo.hatenablog.jp/entry/2016/01/20/200000)
 ## Homework
 ### HASKELL the craft of functional programming Third edition (by Thompson)
 [参考URL](https://www.amazon.co.jp/Haskell-Functional-Programming-International-Computer/dp/0201882957)
-ex3_9.hs ~ ex5_28.hs 2017/12/15
