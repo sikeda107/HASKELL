d056b155dac6e3f77a1c1fb2b622f17cf9d7019b
Sho Ikeda <wakame1121@gmail.com>
2018-01-11 14:10:56 +0900

    modify 3_20

--- HomeWork/ex5_28.hs
+++ HomeWork/ex5_28.hs
@@ -12,14 +12,31 @@ exampleBase = [ ("Alice" , "Tintin") , ("Anna" , "Little Women") , ("Alice" , "A
 borrowers :: Database -> Book -> [Person]
 borrowers db bk = [ fst p | p <- db , snd p == bk]
 
-borrowed :: Database -> Book -> [Bool]
-borrowed db bk = [j == bk | j <- bkdb]
+borrowed :: Database -> Book -> Bool
+borrowed db bk = or [j == bk | j <- bkdb]
  where bkdb = [snd i | i <- db]
 
 numBorrowed :: Database -> Person -> Int
 numBorrowed db pn = length [j | j <- pndb , j == pn ]
  where
   pndb = [fst i | i <- db]
+
+numBorrowed :: Database -> Person -> Int
+numBorrowed db pn = length [j | j <- pndb , j == pn ]
+   where
+    pndb = [fst i | i <- db]
+
+  -- borrowed 以外はオーケーです。borrowed の戻り値は Bool です（教科書を
+  -- 参照のこと）。引数に指定された本が貸出中ならTrue、そうでなければ
+  -- Falseを返します。そのようにするには、池田さんが書いた borrowed の
+  -- 定義をほんのすこしだけ変更するだけでオーケーです。正しい borrowed を
+  -- 書いて再提出してください。
+  --
+  -- borrowers は、fst や snd を使う代わりにパターンマッチを使えば、
+  -- より簡潔に書けます。また、numBorrwed は where を使って前処理を
+  -- しなくても、データベースから取り出したペアの第一要素と pn を
+  -- 比べれば簡単に書けます。
+
 
 main :: IO ()
 main = do
--- HomeWork/ex7_25.hs
+++ HomeWork/ex7_25.hs
@@ -1,3 +1,46 @@
+-- 修正前：
+-- -- ship  Fish & Chips
+-- sublist :: String -> String -> Bool
+-- sublist [] _      = True
+-- sublist _ []      = False
+-- sublist (a:b) c = (elem a c) && (sublist b c)
+--
+-- --
+-- subseq :: String -> String -> Bool
+-- subseq [] _          = True
+-- subseq _ []          = False
+-- subseq (x:xs) (y:ys)
+--  | null xs && x == y                    = True
+--  | null ys && xs /= []                  = False
+--  | x == y && xx == yy && subseq xxs yys = True
+--  | subseq (x:xs) ys                     = True
+--  | otherwise                            = False
+--   where
+--    (xx:xxs) = xs
+--    (yy:yys) = ys
+
+-- コメント：
+ -- sublist の定義は誤りです。xs が ys のsublistになる（sublist xs ys = True）
+ -- のは、ys から（順番を変えずに）０個以上の文字を取り除いて xs が得られる
+ -- ときだけです。池田さんの定義では、sublist "aaa" "a" でもTrueになって
+ -- しまいます。(x:xs)が(y:ys)のsublist になる（sublist (x:xs) (y:ys) = True）
+ -- のは、
+ -- (1) x と y が等しく、xs が ys のsublistになるとき、
+ -- または、
+ -- (2) x と y が異なり、(x:xs) が ys のsublistになるとき
+ -- です。
+ --
+ -- subsequence xs ys は、ys の中に xs が現れる（ys から先頭と末尾の長さ０
+ -- 以上の文字列を取り除くと xs が得られる）ときだけ True になります。
+ -- したがって、subsequence (x:xs) (y:ys) が True になるのは、
+ -- (1) (x:xs) が (y:ys) の先頭に現れる文字列である、すなわち、x と y が
+ --     一致し、xs が ys の先頭に現れる文字列であるとき、
+ -- または、
+ -- (2) (x:xs) が ys のsubsequenceであるとき
+ -- です。
+
+
+-- 修正後：
 -- ship  Fish & Chips
 sublist :: String -> String -> Bool
 sublist [] _      = True
@@ -17,6 +60,7 @@ subseq (x:xs) (y:ys)
   where
    (xx:xxs) = xs
    (yy:yys) = ys
+
 
 main :: IO ()
 main = do
