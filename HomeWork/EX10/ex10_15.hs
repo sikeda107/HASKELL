7d58c5aeec4150276872e75cdaafa35de05c07ab
Sho Ikeda <wakame1121@gmail.com>
2018-01-29 12:49:24 +0900

    make re-ans3

--- HomeWork/EX10/ex10_15.hs
+++ HomeWork/EX10/ex10_15.hs
@@ -1,15 +1,32 @@
 import Prelude hiding (unzip,last,init)
 
-last :: [a] -> a
-last [x] =  x
-last (_:xs) =  last xs
+-- last :: [a] -> a
+-- last [x] =  x
+-- last (_:xs) =  last xs
+--
+-- init :: [a] -> [a]
+-- init [x] =  []
+-- init (x:xs) =  x : init xs
 
-init :: [a] -> [a]
-init [x] =  []
-init (x:xs) =  x : init xs
+-- unzip :: [(a, b)] -> ([a], [b])
+-- unzip xs = (map fst xs, map snd xs)
 
-unzip :: [(a, b)] -> ([a], [b])
-unzip xs = (map fst xs, map snd xs)
+lastf :: a -> [a] -> [a]
+lastf x [] = [x]
+lastf x xs = xs
+
+last xs = foldr lastf [] xs
+
+-- initf :: a -> [a] -> [a]
+-- initf x [] = []
+-- initf x xs = (x:xs)
+--
+-- init xs = foldr initf [] xs
+
+unzipf :: (a,a) -> ([a],[a]) -> ([a],[a])
+unzipf (x,y) (xs,ys) = (x:xs, y:ys)
+
+unzip xs = foldr unzipf ([],[]) xs
 
 main :: IO ()
 main = do
