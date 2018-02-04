c080e807929c43524649c9a4b418f3df52878bc0
Sho Ikeda <wakame1121@gmail.com>
2018-02-04 13:16:33 +0900

    making ex18.3

--- HomeWork/EX18/ex18_3.hs
+++ HomeWork/EX18/ex18_3.hs
@@ -1,7 +1,8 @@
+import Prelude  hiding (repeat)
 -- 18.3 は、do を使った定義と、まったく使わない定義の両方を書いてください。
 -- 18.3 をチェックするときは、以下を評価してみてください。
 
-  repeat (do l <- getLine; return (l == "")) $ putStrLn "Hello"
+  -- repeat (do l <- getLine; return (l == "")) $ putStrLn "Hello"
 
 -- 上の式のように、do を１行で書きたいときは、各行を ; で区切ります。
 -- repeat がうまく定義できていれば、改行を入力するまで Hello が改行付きで
@@ -11,4 +12,17 @@
 -- ちなみに、上の式は以下のような do を使わない式に変換できます。
 -- どちらを評価しても結果は同じです。
 
-  repeat (getLine >>= return . (== "")) $ putStrLn "Hello"
+  -- repeat (getLine >>= return . (== "")) $ putStrLn "Hello"
+
+-- so that "repeat test oper" has the effect of repeating oper until the condition test is "True".
+repeat :: IO Bool -> IO() -> IO()
+repeat iobool io = do
+                      if iobool == True
+                      then return 0
+                      else repeat (do l <- getLine; return (l == "")) $ putStrLn "Hello"
+
+main :: IO ()
+main = do
+  putStrLn "Hello"
+  putStrLn "Hello"
+  -- repeat (do l <- getLine; return (l == "")) $ putStrLn "Hello"
